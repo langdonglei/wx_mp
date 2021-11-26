b68596985c179dd34a5190f59cdac44321fc5faf
langdonglei <langdonglei@icloud.com>
2021-11-26 15:17:56 +0800

    lf

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,38 +1,38 @@
 Page({
-  onLoad:function(){
-    
-  },
-  onChangeShowEnvChoose() {
-    wx.showActionSheet({
-      itemList: this.data.envList.map(i => i.alias),
-      success: (res) => {
-        this.onChangeSelectedEnv(res.tapIndex);
-      },
-      fail(res) {
-        console.log(res.errMsg);
-      },
-      complete: res => {
-        console.log(333)
-      }
-    });
-  },
-  onChangeSelectedEnv(index) {
-    if (this.data.selectedEnv.envId === this.data.envList[index].envId) {
-      return;
+    onLoad: function () {
+
+    },
+    onChangeShowEnvChoose() {
+        wx.showActionSheet({
+            itemList: this.data.envList.map(i => i.alias),
+            success: (res) => {
+                this.onChangeSelectedEnv(res.tapIndex);
+            },
+            fail(res) {
+                console.log(res.errMsg);
+            },
+            complete: res => {
+                console.log(333)
+            }
+        });
+    },
+    onChangeSelectedEnv(index) {
+        if (this.data.selectedEnv.envId === this.data.envList[index].envId) {
+            return;
+        }
+        const powerList = this.data.powerList;
+        powerList.forEach(i => {
+            i.showItem = false;
+        })
+        this.setData({
+            selectedEnv: this.data.envList[index],
+            powerList,
+            haveCreateCollection: false
+        })
+    },
+    jump(e) {
+        wx.navigateTo({
+            url: e.currentTarget.dataset.page
+        });
     }
-    const powerList = this.data.powerList;
-    powerList.forEach(i => {
-      i.showItem = false;
-    })
-    this.setData({
-      selectedEnv: this.data.envList[index],
-      powerList,
-      haveCreateCollection: false
-    })
-  },
-  jump(e) {
-    wx.navigateTo({
-      url: e.currentTarget.dataset.page
-    });
-  }
 })
